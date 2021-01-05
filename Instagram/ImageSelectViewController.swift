c7d8901becbe8132836a00031f8fa730f1b1dab7
ayaka <nobe.ayaka.0428@gmail.com>
2021-01-05 09:57:01 +0900

    ログイン、写真アクセスとカメラ呼び出し

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -8,23 +8,59 @@
 
 import UIKit
 
-class ImageSelectViewController: UIViewController {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //ライブラリ（カメラロール）を指定してピッカーを開く
+    @IBAction func handleLibraryButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    //カメラを指定してピッカーを開く
+    @IBAction func handleCameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+        //画面を閉じる
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //写真撮影/選択した時に呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //撮影/選択された画像を取得
+        if info[.originalImage] != nil{
+            let image = info[.originalImage] as! UIImage
+            
+            //後でCLImageEditorライブラリで加工する
+            print("DEBUG_PRINT: image = \(image)")
+        }
     }
-    */
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //ImageSelectViewController画面を閉じてタブ画面に戻る
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    
 
 }
